f750dc66373d20635c8c3c95f508514445a655cb
chitra <yandachitralekha255@gmail.com>
2025-08-25 09:34:12 +0530

    TestNg

--- src/test/java/com/orangehrm/stepdefinitions/TestRunner.java
+++ src/test/java/com/orangehrm/stepdefinitions/TestRunner.java
@@ -1,8 +1,15 @@
 package com.orangehrm.stepdefinitions;
 
+import org.junit.runner.RunWith;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 
-
+@RunWith(Cucumber.class)
+@CucumberOptions(features="E:\\Eclipse\\Java_Selenium\\src\\test\\resources\\Features"
+,glue= {"com.orangehrm.stepdefinitions;"}
+,dryRun=false
+,monochrome=true)
 public class TestRunner {
 	
 
